c7775d27dd73de99c860c6ed2335e60fcde74652
yerimmseo <yerim7386@naver.com>
2021-12-23 16:18:38 +0900

    게시글 등록 기능 생성

--- src/main/java/com/practice/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/practice/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.practice.book.springboot.service.posts;
 
 import com.practice.book.springboot.domain.posts.Posts;
 import com.practice.book.springboot.domain.posts.PostsRepository;
+import com.practice.book.springboot.web.dto.PostsListResponseDto;
 import com.practice.book.springboot.web.dto.PostsResponseDto;
 import com.practice.book.springboot.web.dto.PostsSaveRequestDto;
 import com.practice.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 대신 생성해줌
 @Service
@@ -44,5 +48,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // readOnly = true : 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // .map(posts -> new PostsListResponseDto(posts)) : 넘어온 posts의 Stream을 통해 PostsListResponseDto 변환 -> List로 반환
+                .collect(Collectors.toList());
     }
 }
